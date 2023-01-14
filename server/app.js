83160c8fd3e4acdfa85702d8710d721eb85b0cef
Claire McGuigan <mcguigan.claire@outlook.com>
2023-01-14 16:05:57 -0500

    Linking NewsAPI call and cohere model
    Linking the news API call and linking the co:here model. Sending back results from get request

--- server/app.js
+++ server/app.js
@@ -69,17 +69,16 @@ app.get('/api/cohere/:text', (req, res)=>{
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/:keyword', (req,res) => {
-    console.log(req.get('user-agent'));
-    let data = NewsAPICall(req.params.keyword,req);
-    res.send(data);
+app.get('/api/:keyword', async(req,res) => {
+    let userAgent = req.get('user-agent');
+    let data = await NewsAPICall(req.params.keyword,userAgent,res);
 });
 
-function NewsAPICall(keyword, req) {
-    const userAgent = req.get('user-agent');
+async function NewsAPICall(keyword, userAgent,res) {
+    //const userAgent = req.get('user-agent');
     const options = {
         host: 'newsapi.org',
-        path: `/v2/everything?q=${keyword}&apiKey=fb4ca3c8afb547a0a3b2b74918d9ae82`,
+        path: `/v2/everything?q=${keyword}&pageSize=96&apiKey=fb4ca3c8afb547a0a3b2b74918d9ae82`,
         headers: {
           'User-Agent': userAgent
         }
@@ -94,15 +93,14 @@ function NewsAPICall(keyword, req) {
                 data += chunk;
             }
         });
-        response.on('end', function () {
+        response.on('end', async function () {
             const newsData = JSON.parse(data);
             let arrayOfHeadlines = [];
             for(let item of newsData.articles){
                 arrayOfHeadlines.push(item.title);
             }
-            console.log(arrayOfHeadlines);
-            let classification = classifyNewsArticles(arrayOfHeadlines);
-            console.log(classification);
+    
+            res.send({data: await cohereNewsArticles(arrayOfHeadlines)});
         });
     });
 }
@@ -114,17 +112,41 @@ app.listen(3100, ()=>{
 
 const cohere = require('cohere-ai');
 cohere.init('CkDyobTPuxXHETB5lRaIjEQgUntgXTivHbsGQi51');
+
 function coherefn(input){
-(async () => {
-  const response = await cohere.classify({
-    model: 'small',
-    inputs: [input],
-    examples: [{"text": "Idaho Murders Suspect Felt No Emotion and Little Remorse as a Teen", "label": "negative"}, {"text": "Idaho murders  latest news: Bryan Kohbergers chilling past comments come to light", "label": "negative"}, {"text": "Idaho murders timeline: What we know about the slayings of four students", "label": "neutral"}, {"text": "What we know so far about the investigation into the Idaho college student murders", "label": "neutral"}, {"text": "Hope Within the Heartbreak: Finding Good News in Ukraine", "label": "positive"}, {"text": "Sunak confirms UK will send tanks to Ukraine ‘to push Russian troops back’", "label": "positive"}, {"text": "Germany opens its 2nd liquefied natural gas terminal", "label": "positive"}, {"text": "BBC PROMS 2022: UKRAINIAN REFUGEE ORCHESTRA AMONG THE LINE-UP", "label": "positive"}, {"text": "It’s time for the international community to address this crisis with greater honesty about the key players and solutions", "label": "positive"}, {"text": "Ask the AI program a question, as millions have in recent weeks, and it will do its best to respond", "label": "positive"}, {"text": "Why tech insiders are so excited about ChatGPT, a chatbot that answers questions and writes essays", "label": "positive"}, {"text": "What’s the point of personal statements when ChatGPT can say it so much better?", "label": "positive"}, {"text": "ChatGPT Has Investors Drooling—but Can It Bring Home the Bacon?", "label": "positive"}, {"text": "From changing your diet to offering refugees a room, here are five things you can do to help the Ukrainian people", "label": "positive"}, {"text": "First commercial drone home delivery service in U.S. takes flight", "label": "positive"}, {"text": "10 INNOVATIVE TECHNOLOGIES WITH POTENTIAL IMPACT FOR BUSINESS", "label": "positive"}, {"text": "From advancements in artificial intelligence technologies to 3D printing, 2018 has been a major year in tech. As cutting-edge technologies reach the market and are integrated, business strategies may evolve as well.", "label": "positive"}, {"text": "We live in an era called the information age. New technology is emerging every day to make life simpler, more advanced and better for everyone. The rate at which technology is evolving is almost exponential today. For business organisations, new technology helps to reduce costs, enhance customer experiences and increase profits.", "label": "positive"}]
-  });
-  for(id in response.body.classifications){
-    console.log(response.body.classifications[id].prediction)
-    return (response.body.classifications[id].prediction)
-  }
-  
-})();
+    (async () => {
+    const response = await cohere.classify({
+        model: 'small',
+        inputs: [input],
+        examples: [{"text": "Idaho Murders Suspect Felt No Emotion and Little Remorse as a Teen", "label": "negative"}, {"text": "Idaho murders  latest news: Bryan Kohbergers chilling past comments come to light", "label": "negative"}, {"text": "Idaho murders timeline: What we know about the slayings of four students", "label": "neutral"}, {"text": "What we know so far about the investigation into the Idaho college student murders", "label": "neutral"}, {"text": "Hope Within the Heartbreak: Finding Good News in Ukraine", "label": "positive"}, {"text": "Sunak confirms UK will send tanks to Ukraine ‘to push Russian troops back’", "label": "positive"}, {"text": "Germany opens its 2nd liquefied natural gas terminal", "label": "positive"}, {"text": "BBC PROMS 2022: UKRAINIAN REFUGEE ORCHESTRA AMONG THE LINE-UP", "label": "positive"}, {"text": "It’s time for the international community to address this crisis with greater honesty about the key players and solutions", "label": "positive"}, {"text": "Ask the AI program a question, as millions have in recent weeks, and it will do its best to respond", "label": "positive"}, {"text": "Why tech insiders are so excited about ChatGPT, a chatbot that answers questions and writes essays", "label": "positive"}, {"text": "What’s the point of personal statements when ChatGPT can say it so much better?", "label": "positive"}, {"text": "ChatGPT Has Investors Drooling—but Can It Bring Home the Bacon?", "label": "positive"}, {"text": "From changing your diet to offering refugees a room, here are five things you can do to help the Ukrainian people", "label": "positive"}, {"text": "First commercial drone home delivery service in U.S. takes flight", "label": "positive"}, {"text": "10 INNOVATIVE TECHNOLOGIES WITH POTENTIAL IMPACT FOR BUSINESS", "label": "positive"}, {"text": "From advancements in artificial intelligence technologies to 3D printing, 2018 has been a major year in tech. As cutting-edge technologies reach the market and are integrated, business strategies may evolve as well.", "label": "positive"}, {"text": "We live in an era called the information age. New technology is emerging every day to make life simpler, more advanced and better for everyone. The rate at which technology is evolving is almost exponential today. For business organisations, new technology helps to reduce costs, enhance customer experiences and increase profits.", "label": "positive"}]
+    });
+    for(id in response.body.classifications){
+        console.log(response.body.classifications[id].prediction)
+        return (response.body.classifications[id].prediction)
+    }
+    })();
+}
+
+async function cohereNewsArticles(input){
+    const response = await cohere.classify({
+        model: 'small',
+        inputs: input,
+        examples: [{"text": "Idaho Murders Suspect Felt No Emotion and Little Remorse as a Teen", "label": "negative"}, {"text": "Idaho murders  latest news: Bryan Kohbergers chilling past comments come to light", "label": "negative"}, {"text": "Idaho murders timeline: What we know about the slayings of four students", "label": "neutral"}, {"text": "What we know so far about the investigation into the Idaho college student murders", "label": "neutral"}, {"text": "Hope Within the Heartbreak: Finding Good News in Ukraine", "label": "positive"}, {"text": "Sunak confirms UK will send tanks to Ukraine ‘to push Russian troops back’", "label": "positive"}, {"text": "Germany opens its 2nd liquefied natural gas terminal", "label": "positive"}, {"text": "BBC PROMS 2022: UKRAINIAN REFUGEE ORCHESTRA AMONG THE LINE-UP", "label": "positive"}, {"text": "It’s time for the international community to address this crisis with greater honesty about the key players and solutions", "label": "positive"}, {"text": "Ask the AI program a question, as millions have in recent weeks, and it will do its best to respond", "label": "positive"}, {"text": "Why tech insiders are so excited about ChatGPT, a chatbot that answers questions and writes essays", "label": "positive"}, {"text": "What’s the point of personal statements when ChatGPT can say it so much better?", "label": "positive"}, {"text": "ChatGPT Has Investors Drooling—but Can It Bring Home the Bacon?", "label": "positive"}, {"text": "From changing your diet to offering refugees a room, here are five things you can do to help the Ukrainian people", "label": "positive"}, {"text": "First commercial drone home delivery service in U.S. takes flight", "label": "positive"}, {"text": "10 INNOVATIVE TECHNOLOGIES WITH POTENTIAL IMPACT FOR BUSINESS", "label": "positive"}, {"text": "From advancements in artificial intelligence technologies to 3D printing, 2018 has been a major year in tech. As cutting-edge technologies reach the market and are integrated, business strategies may evolve as well.", "label": "positive"}, {"text": "We live in an era called the information age. New technology is emerging every day to make life simpler, more advanced and better for everyone. The rate at which technology is evolving is almost exponential today. For business organisations, new technology helps to reduce costs, enhance customer experiences and increase profits.", "label": "positive"}]
+    });
+    //console.log(response.body.classifications);
+    let total = 0;
+    let positive = 0;
+    let confidence = 0.00;
+    for(let result of response.body.classifications){
+        total += 1;
+        if(result.prediction == "positive"){
+            positive += 1;
+            confidence += result.confidence;
+        }
+    }
+    let returnObj = {
+        positiveRatio: (positive/total)*100,
+        confidence: confidence/total
+    }
+    return returnObj;
 }

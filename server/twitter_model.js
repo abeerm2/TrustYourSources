8ee2a769b4421882a75eeebae6797019a365994f
Claire McGuigan <mcguigan.claire@outlook.com>
2023-01-14 10:04:11 -0500

    Express server
    Creating express server and linking to react app. Creating skeleton files for the two co:here models

---
+++ server/twitter_model.js
@@ -0,0 +1,18 @@
+const cohere = require('cohere-ai');
+cohere.init(process.env.API_KEY);
+
+const POSITIVE_EXAMPLES = [];
+const NEGATIVE_EXAMPLES = [];
+
+async function classifySocialMedia(input) {
+    const response = await cohere.classify({
+        model: "medium",
+        taskDescription: "",
+        outputIndicator: "",
+        inputs: [input],
+        examples: POSITIVE_EXAMPLES.map((positive) => ({text: positive, label: "positive"})).concat(
+            NEGATIVE_EXAMPLES.map((negative) => ({text: negative, label: "negative"})))
+    })
+
+    return response.body.classifications[0];
+}
